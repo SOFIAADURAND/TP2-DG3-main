e1e76f5b80436084f2538606f2f25952c6bd53aa
sofi durand <sofi-durand@hotmail.com>
2024-07-08 21:24:28 -0300

    HICE UN CONTADOR

--- assets/js/main.js
+++ assets/js/main.js
@@ -210,3 +210,25 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+// Fecha hacia la cual quieres contar
+const countDownDate = new Date("August 12, 2024 19:00:00").getTime();
+
+function countdownFunction() {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+}
+
+// Actualiza el contador cada segundo
+setInterval(countdownFunction, 1000);
+
+// Ejecuta la función al cargar la página para evitar el retraso inicial
+countdownFunction();
